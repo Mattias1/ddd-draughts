f78b09d98baf47bb0e039ab40b41662a95201ffc
Mattias1 <1116150+Mattias1@users.noreply.github.com>
2020-06-16 13:20:46 +0200

    Setup the ASP.NET Core 3.1 framework

---
+++ Draughts/Startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Draughts {
+    public class Startup {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            else {
+                app.UseExceptionHandler("/StaticPages/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=StaticPages}/{action=Home}/{id?}");
+            });
+        }
+    }
+}
