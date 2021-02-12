2c8d807bda5dc26df373cab5a394d1f5481d918e
Mattias <1116150+Mattias1@users.noreply.github.com>
2021-02-12 14:47:15 +0100

    Change board comments format, as auto-indent detection can't deal with it.

--- Draughts.Test/Domain/GameAggregate/PossibleMovesCalculatorTest.cs
+++ Draughts.Test/Domain/GameAggregate/PossibleMovesCalculatorTest.cs
@@ -9,51 +9,51 @@ namespace Draughts.Test.Domain.GameAggregate {
         // TODO: This assumes man can capture backwards and more of this international sillyness ;)
         [Fact]
         public void ManMoves() {
-            /*|_|4|_|.|
-              |.|_|.|_|
-              |_|5|_|.|
-              |.|_|5|_|*/
+            // |_|4|_|.|
+            // |.|_|.|_|
+            // |_|5|_|.|
+            // |.|_|5|_|
             CalculatePossibleMoves("40005005", Color.White).Should().BeEquivalentTo("5-3", "5-4", "8-6");
         }
 
         [Fact]
         public void ManCaptures() {
-            /*|_|.|_|4|
-              |.|_|5|_|
-              |_|.|_|4|
-              |.|_|5|_|*/
+            // |_|.|_|4|
+            // |.|_|5|_|
+            // |_|.|_|4|
+            // |.|_|5|_|
             CalculatePossibleMoves("04050405", Color.Black).Should().BeEquivalentTo("2x5", "6x1");
         }
 
         [Fact]
         public void NoMovesWhenCapturing() {
-            /*|_|4|_|.|
-              |.|_|4|_|
-              |_|5|_|4|
-              |.|_|.|_|*/
+            // |_|4|_|.|
+            // |.|_|4|_|
+            // |_|5|_|4|
+            // |.|_|.|_|
             CalculatePossibleMoves("40045400", Color.Black).Should().BeEquivalentTo("4x7");
         }
 
         [Fact]
         public void KingMoves() {
-            /*|_|7|_|.|_|4|
-              |.|_|.|_|.|_|
-              |_|.|_|6|_|.|
-              |6|_|.|_|5|_|
-              |_|4|_|.|_|4|
-              |6|_|.|_|5|_|*/
+            // |_|7|_|.|_|4|
+            // |.|_|.|_|.|_|
+            // |_|.|_|6|_|.|
+            // |6|_|.|_|5|_|
+            // |_|4|_|.|_|4|
+            // |6|_|.|_|5|_|
             CalculatePossibleMoves("704 000 060 605 404 605", Color.Black)
                 .Should().BeEquivalentTo("3-6", "8-5", "8-6", "8-11", "10-7", "10-5", "10-2", "13-17");
         }
 
         [Fact]
         public void KingCaptures() {
-            /*|_|.|_|.|_|.|
-              |.|_|5|_|.|_|
-              |_|6|_|4|_|4|
-              |.|_|.|_|7|_|
-              |_|.|_|4|_|.|
-              |7|_|.|_|.|_|*/
+            // |_|.|_|.|_|.|
+            // |.|_|5|_|.|_|
+            // |_|6|_|4|_|4|
+            // |.|_|.|_|7|_|
+            // |_|.|_|4|_|.|
+            // |7|_|.|_|.|_|
             CalculatePossibleMoves("000 050 644 007 040 700", Color.White)
                 .Should().BeEquivalentTo("5x10", "12x17", "16x6", "16x3");
         }
@@ -68,10 +68,10 @@ namespace Draughts.Test.Domain.GameAggregate {
 
         [Fact]
         public void OnlyCaptureFromRestrictedFieldWhenChaining() {
-            /*|_|.|_|.|
-              |.|_|5|_|
-              |_|4|_|4|
-              |.|_|5|_|*/
+            // |_|.|_|.|
+            // |.|_|5|_|
+            // |_|4|_|4|
+            // |.|_|5|_|
             var board = BoardPosition.FromString("00054405");
             var posisbleMoves = PossibleMoveCalculator.ForChainCaptures(board, new Square(5))
                 .Calculate()
